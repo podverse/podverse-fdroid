3a0fdd51344db0dbc07ad3df7572ba5a2d3e77fb
Mitch Downey <mitchldowney@gmail.com>
2020-09-26 01:44:35 -0500

    Update ScanQRCodeScreen.tsx

--- src/screens/ScanQRCodeScreen.tsx
+++ src/screens/ScanQRCodeScreen.tsx
@@ -1,112 +1,12 @@
 // @flow
 
-import React, { useState } from 'react'
-import { Alert, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { RNCamera } from 'react-native-camera'
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
 import { translate } from '../lib/i18n'
 import { PV } from '../resources'
-import { getAddByRSSPodcastLocally } from '../services/parser'
-import { saveSpecialUserInfo } from '../services/user'
-import { addAddByRSSPodcast } from '../state/actions/parser'
 
-type Props = {}
-
-export const ScanQRCodeScreen = (props: Props) => {
-  const [scanned, setScanned] = useState(false)
-  const { navigate, dismiss } = props.navigation
-
-  const parsePayload = (qrData = '') => {
-    if (!qrData || !qrData.startsWith('https://')) {
-      throw new Error(translate('Invalid or missing QR data'))
-    }
-
-    const parsedData = qrData.split('?')
-
-    let userInfo: object | null = null
-
-    if (parsedData.length > 1) {
-      userInfo = {}
-      const params = parsedData[1].split('&')
-
-      params.forEach((param) => {
-        const pair = param.split('=')
-        if (pair.length < 2) {
-          throw new Error(translate('Invalid params in feed url'))
-        }
-
-        if (userInfo) userInfo[pair[0]] = pair[1]
-      })
-    }
-
-    return {
-      feedUrl: parsedData[0],
-      userInfo
-    }
-  }
-
-  const showQRRead = async (scannedData: string) => {
-    try {
-      const parsedData = parsePayload(scannedData)
-
-      if (parsedData.userInfo) {
-        await saveSpecialUserInfo(parsedData.userInfo)
-      }
-
-      await addAddByRSSPodcast(parsedData.feedUrl)
-      const podcast = await getAddByRSSPodcastLocally(parsedData.feedUrl)
-
-      navigate(PV.RouteNames.PodcastScreen, {
-        podcast,
-        addByRSSPodcastFeedUrl: podcast.addByRSSPodcastFeedUrl
-      })
-    } catch (error) {
-      console.log(error)
-      Alert.alert(translate('QR Code Error'), error.message || error, [
-        {
-          text: translate('OK'),
-          onPress: () => {
-            setScanned(false)
-          }
-        }
-      ])
-    }
-  }
-
-  return (
-    <SafeAreaView style={styles.view}>
-      <RNCamera
-        style={styles.preview}
-        onBarCodeRead={(event) => {
-          if (!scanned) {
-            setScanned(true)
-
-            // Give it a nice illusion of processing
-            setTimeout(() => showQRRead(event.data), 300)
-          }
-        }}
-        captureAudio={false}
-        barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}>
-        <View style={styles.verticalFiller}>
-          <View style={styles.horizontalFiller} />
-          <View style={styles.fillerRow}>
-            <View style={styles.horizontalRowFiller} />
-            <View style={styles.innerCutout} />
-            <View style={styles.horizontalRowFiller} />
-          </View>
-          <View style={styles.contentContainer}>
-            <Text style={styles.instructions} allowFontScaling={false}>
-              {scanned ? translate('Processing') : translate('Scan a valid QR code')}
-            </Text>
-            {!scanned && (
-              <TouchableOpacity style={styles.dismissButton} onPress={() => dismiss()}>
-                <Text style={styles.dismissButtonText}>{translate('CANCEL')}</Text>
-              </TouchableOpacity>
-            )}
-          </View>
-        </View>
-      </RNCamera>
-    </SafeAreaView>
-  )
+export const ScanQRCodeScreen = () => {
+  return <SafeAreaView style={styles.view} />
 }
 
 ScanQRCodeScreen.navigationOptions = () => ({
