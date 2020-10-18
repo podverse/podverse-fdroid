8457c5f47e7f0cc875c4607bfa54e6cb7af04342
Mitch Downey <mitchldowney@gmail.com>
2020-10-17 22:22:49 -0500

    Update azure-pipelines-post-build.sh

--- azure-pipelines-post-build.sh
+++ azure-pipelines-post-build.sh
@@ -5,7 +5,7 @@ pwd
 
 echo "======= Browserstack upload start ======="
 
-APP_URL=$(curl -u "$BS_USERNAME:$BS_KEY" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$OUTPUT_PATH")
+APP_URL="helloworld"
 
 jsonval() {
     temp=`echo $APP_URL | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w "app_url"| cut -d":" -f1- | sed -e 's/^ *//g' -e 's/ *$//g'`
